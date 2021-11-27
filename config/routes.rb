2e25acd4caf01d351648134d26cdd67e7d8280e5
caisilus <serangor@gmail.com>
2021-11-27 19:00:41 +0300

    Add routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  root 'course#index'
 
+  get '/auth/new', to: 'auth#new'
+  post '/auth', to: "auth#create"
+  post '/auth/microsoft', to: 'auth#microsoft'
+  delete '/auth', to: 'auth#destroy'
+
+  post '/users', to: 'users#create'
+
+  get '/course', to: 'course#index'
+  get '/course/labs/:lab_id', to: 'labs#show'
+  get '/labs', to: 'labs#index' # information about labs
+
+  # for logged in users
+  get '/results', to: 'results#index'
 end
