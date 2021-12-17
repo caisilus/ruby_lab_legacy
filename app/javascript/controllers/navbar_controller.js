14263a507ecccac55e715be1eac1b3b88bb9626e
caisilus <serangor@gmail.com>
2021-12-18 00:34:20 +0300

    Fix active navbar link initialization

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,11 +2,20 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
     static targets = [ "section" ]
-    static values = { index: { type: Number, default: 0 } }
+    static values = { index: { type: Number, default: 0 }, navname: { type: String, default: "default" } }
 
     initialize() {
+        this.nav_names = this.element.dataset.names.split("|");
+        this.initialize_index();
         const section = this.get_last_section();
         section.classList.toggle("active");
+    }
+
+    initialize_index() {
+        this.indexValue = this.nav_names.indexOf(this.navnameValue);
+        if (this.indexValue === -1) {
+            this.indexValue = 0;
+        }
     }
 
     enable_section(event){
